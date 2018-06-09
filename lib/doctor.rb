d72e49d48953843de4d8aa3eac086bfd82a5810d
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-06-09 09:05:03 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,7 +12,7 @@ class Doctor
     @@all
   end
 
-  def new_appointment(patient, doctor, date)
+  def new_appointment(patient, date)
     appointment = Appointment.new(patient, doctor=self, date)
   end
 
