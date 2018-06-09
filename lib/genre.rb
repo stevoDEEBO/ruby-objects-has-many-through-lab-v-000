25f843ed14ee4d018008a288bebc534e277952f7
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-06-09 08:22:04 +0000

    automatically backed up by learn

---
+++ lib/genre.rb
@@ -0,0 +1,18 @@
+class Genre
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, genre)
+    song.genre = self
+  end
